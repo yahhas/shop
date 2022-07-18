718adc4df7353188a445c4dbaf68452534b37727
Sergei <light.heaven@yandex.ru>
2022-07-18 11:48:42 +0300

    added: buy section, footer block

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-	$('.bike-slider').slick({
+	$('.bike-slider, .slider__items').slick({
 		arrows: false,
 		dots: true,
 		fade: true,
@@ -7,3 +7,4 @@ $(function(){
 		autoplaySpeed: 2000
 	})
 });
+
